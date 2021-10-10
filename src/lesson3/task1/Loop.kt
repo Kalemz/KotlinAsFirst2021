2e20a302d5eeee70e80ac33da57300306227e837
Михаил Зотов(lesson 1) <zotov.ma@edu.spbstu.ru>
2021-10-10 23:38:08 +0300

    Zotov

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -3,6 +3,7 @@
 package lesson3.task1
 
 import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sqrt
 
 // Урок 3: циклы
@@ -153,7 +154,12 @@ return m*n
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    for (c in 2..min(m,n)){
+        if ((n%c==0) and (m%c==0)) return false
+
+    }
+return true}
 
 /**
  * Средняя (3 балла)
